ce5ab91647892ec22e1402883d8245b498750c54
cooksoundscapes <paulobmedina@gmail.com>
2022-03-11 16:52:52 -0300

    boilerplate ok

---
+++ webpack.common.js
@@ -0,0 +1,66 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'main.js',
+    clean: true,
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [{
+          loader: "css-loader",
+          options: {
+            importLoaders: 1,
+            modules: {
+              //localIdentName purpose is to add unique class-name to css modules!
+              localIndentName: "[name]__[local]__[hash:base64:5]"
+            }
+          }
+        }],
+        include: /\.module\.css$/
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
+        exclude: /\.module\.css$/
+      },
+      {
+        test: /\.(png|jpe?g|gif|wav)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          }
+        ]
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({
+    filename: 'index.html',
+    template: 'src/index.html'
+  })],
+};
+  
